58edc8acd562c5113495c2be1b70b890a92c3f1f
thomas-tahk <tomtahk27@gmail.com>
2025-03-20 20:33:25 -0600

    added reactui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { createRoot} from "react-dom/client"
 import { StrictMode } from "react";
 import initGame from "./initGame";
+import ReactUI from "./ReactUI";
 
 const ui = document.getElementById('ui');
 const root = createRoot(ui)
 root.render(
   <StrictMode>
-    <h1>Hello World!</h1>
+    <ReactUI />
   </StrictMode>
 );
 
