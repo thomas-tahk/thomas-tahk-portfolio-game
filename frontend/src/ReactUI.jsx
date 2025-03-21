1dda594f0e7db7c42a47e209fdc892e3d7ebd43a
thomas-tahk <tomtahk27@gmail.com>
2025-03-20 20:30:07 -0600

    ReactUI intiated

---
+++ frontend/src/ReactUI.jsx
@@ -0,0 +1,8 @@
+export default function ReactUI() {
+    return (
+        <>
+            <p>Tap/Click around to move</p>
+            {/* TODO: Bring the UI components here */}
+        </>
+    )
+}
--- frontend/src/initGame.js
+++ frontend/src/initGame.js
@@ -3,5 +3,4 @@ import makeKaplayCtx from "./kaplayCtx";
 export default async function initGame() {
     const k = makeKaplayCtx()
     k.loadSprite("player", "./sprites/player.png", {})
-
 }
