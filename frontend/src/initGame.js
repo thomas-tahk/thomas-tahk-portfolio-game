25c657eb2e84c5e42ac2a37a56d65e474d36787b
thomas-tahk <tomtahk27@gmail.com>
2025-03-25 23:09:08 -0600

    fixed links to public assets

--- frontend/src/initGame.js
+++ frontend/src/initGame.js
@@ -1,9 +1,10 @@
 import makeKaplayCtx from "./kaplayCtx";
+import { PALETTE } from "./constants";
 
 export default async function initGame() {
     const k = makeKaplayCtx()
     // player sprite and designating which frames are for which animations
-    k.loadSprite("player", "./sprites/player.png", {
+    k.loadSprite("player", "/sprites/player.png", {
         sliceX: 4, // number of frames per row
         sliceY: 8, // number of frames per column
         anims: {
@@ -27,27 +28,27 @@ export default async function initGame() {
         }
     })
     // loading various fonts and sprites to be used in the project
-    k.loadFont("ibm-regular", "./fonts/IBMPlexSans-Regular.ttf");
-    k.loadFont("ibm-bold", "./fonts/IBMPlexSans-Bold.ttf");
-    k.loadSprite("github-logo", "./logos/github-logo.png");
-    k.loadSprite("linkedin-logo", "./logos/linkedin-logo.png");
-    k.loadSprite("youtube-logo", "./logos/youtube-logo.png");
-    k.loadSprite("x-logo", "./logos/x-logo.png");
-    k.loadSprite("substack-logo", "./logos/substack-logo.png");
-    k.loadSprite("javascript-logo", "./logos/js-logo.png");
-    k.loadSprite("typescript-logo", "./logos/ts-logo.png");
-    k.loadSprite("react-logo", "./logos/react-logo.png");
-    k.loadSprite("nextjs-logo", "./logos/nextjs-logo.png");
-    k.loadSprite("postgres-logo", "./logos/postgres-logo.png");
-    k.loadSprite("html-logo", "./logos/html-logo.png");
-    k.loadSprite("css-logo", "./logos/css-logo.png");
-    k.loadSprite("tailwind-logo", "./logos/tailwind-logo.png");
-    k.loadSprite("python-logo", "./logos/python-logo.png");
-    k.loadSprite("email-logo", "./logos/email-logo.png");
+    k.loadFont("ibm-regular", "/fonts/IBMPlexSans-Regular.ttf");
+    k.loadFont("ibm-bold", "/fonts/IBMPlexSans-Bold.ttf");
+    k.loadSprite("github-logo", "/logos/github-logo.png");
+    k.loadSprite("linkedin-logo", "/logos/linkedin-logo.png");
+    k.loadSprite("youtube-logo", "/logos/youtube-logo.png");
+    k.loadSprite("x-logo", "/logos/x-logo.png");
+    k.loadSprite("substack-logo", "/logos/substack-logo.png");
+    k.loadSprite("javascript-logo", "/logos/js-logo.png");
+    k.loadSprite("typescript-logo", "/logos/ts-logo.png");
+    k.loadSprite("react-logo", "/logos/react-logo.png");
+    k.loadSprite("nextjs-logo", "/logos/nextjs-logo.png");
+    k.loadSprite("postgres-logo", "/logos/postgres-logo.png");
+    k.loadSprite("html-logo", "/logos/html-logo.png");
+    k.loadSprite("css-logo", "/logos/css-logo.png");
+    k.loadSprite("tailwind-logo", "/logos/tailwind-logo.png");
+    k.loadSprite("python-logo", "/logos/python-logo.png");
+    k.loadSprite("email-logo", "/logos/email-logo.png");
     // a shader is a program that runs on gpu that changes color of pixel on screen
     // kinda like a printing press for computer screens
     // we're using a fragment shader program written in glsl specific to kaplay
-    k.loadShaderURL("tiledPattern", null, "./shaders/tiledPattern.frag");
+    k.loadShaderURL("tiledPattern", null, "/shaders/tiledPattern.frag");
     // gameobject that displays the shader
     const tiledBackground = k.add([
         k.uvquad(k.width(), k.height()),
@@ -62,4 +63,10 @@ export default async function initGame() {
     k.pos(0),
     k.fixed()
     ])
+
+    k.onResize(() => {
+        tiledBackground.width = k.width()
+        tiledBackground.height = k.height()
+        tiledBackground.uniform.u_aspect = k.width() / k.height()
+    })
 }
