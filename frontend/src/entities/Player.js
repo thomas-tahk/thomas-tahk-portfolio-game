8e15c63cc19f69a3eac68d2e8c4da110abd28a03
thomas-tahk <tomtahk27@gmail.com>
2025-03-28 23:32:21 -0600

    added some movement when holding down mouse, lot left to do still

--- frontend/src/entities/Player.js
+++ frontend/src/entities/Player.js
@@ -16,6 +16,40 @@ export default function makePlayer(k, posVec2, speed) {
     ])
 
     // TODO: player controls, etc...
-    
+    let isMouseDown = false
+    const game = document.getElementById("game");
+    game.addEventListener("focusout", () => {
+        isMouseDown = false
+    })
+    game.addEventListener("mousedown", () => {
+        isMouseDown = true
+    })
+    game.addEventListener("mouseup", () => {
+        isMouseDown = false
+    })  
+    game.addEventListener("touchstart", (e) => {
+        isMouseDown = true
+    })
+    game.addEventListener("touchend", (e) => {
+        isMouseDown = false
+    })
+
+    player.onUpdate(() => {
+        if (!k.camPos().eq(player.pos)) {
+            k.tween(k.camPos(), player.pos, 0.2, (newPost) => k.camPos(newPost),
+        k.easings.linear
+    )
+        }
+
+        player.direction = k.vec2(0, 0)
+        const worldMousePos = k.toWorld(k.mousePos())
+        if (isMouseDown) {
+            player.direction = worldMousePos.sub(player.pos).unit()
+        }
+        // Todo implement animations
+
+        player.move(player.direction.scale(speed))
+    })
+
     return player
 }
