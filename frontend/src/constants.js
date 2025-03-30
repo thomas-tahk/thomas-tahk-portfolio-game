e484f18a433c01bee0749b1dc300d44f79fed986
thomas-tahk <tomtahk27@gmail.com>
2025-03-30 01:57:16 -0600

    added a few constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -3,3 +3,7 @@ export const PALETTE = {
     color2: "#4ee8bf",
     color3: "#dbfff5"
 }
+
+export const DIAGONAL_FACTOR = 1/ Math.sqrt(2)
+export const ZOOM_MAX_BOUND = 2
+export const ZOOM_MIN_BOUND = 0.2
