6385aef8f23722b76788ff379ddd901659f21ef1
thomas-tahk <tomtahk27@gmail.com>
2025-04-06 18:48:31 -0600

    some camera zoom controls

---
+++ frontend/src/reactComponents/CameraController.jsx
@@ -0,0 +1,18 @@
+import { ZOOM_MAX_BOUND, ZOOM_MIN_BOUND } from "../constants"
+
+export default function CameraController() {
+    const [camZoomValue, setCamZoomValue] = useAtom(cameraZoomValueAtom)
+
+    return (
+        <div>
+            <button
+            onClick={() => {
+                const newZoomValue = camZoomValue + 0.2
+                if (newZoomValue <= ZOOM_MAX_BOUND && newZoomValue >= ZOOM_MIN_BOUND) {
+                    setCamZoomValue(newZoomValue)
+                }
+            }}>+</button>
+            <button>-</button>
+        </div>
+    )
+}
