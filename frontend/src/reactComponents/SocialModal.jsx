f65db3652112464c690cfd6c2e1a3c4d57a6ea03
thomas-tahk <tomtahk27@gmail.com>
2025-04-15 23:02:49 -0600

    some new component files made, button functions and styling

---
+++ frontend/src/reactComponents/SocialModal.jsx
@@ -0,0 +1,42 @@
+import { useAtom } from "jotai";
+import { isSocialModalVisibleAtom, selectedLinkAtom } from "../store";
+
+export default function SocialModal() { 
+    const [isVisible, setIsVisible] = useAtom(isSocialModalVisibleAtom)
+    const selectedLink = useAtomValue(selectedLinkAtom)
+    const selectedLinkDescription = useAtomValue(selectedLinkAtom)
+
+    const buttons = [
+        { id: 0,
+            name: "Yes",
+            handler: () => {
+                window.open(selectedLink, "_blank")
+                setIsVisible(false)
+            }, 
+        }, 
+        {
+            id: 1,
+            name: "No",
+            handler: () => {
+                setIsVisible(false)
+            },
+        },
+    ]
+
+    return isVisible && (
+        <div className="modal">
+            <div className="modal-content">
+                <h1>Do you want to open this link?</h1>
+                <span>{selectedLink}</span>
+                <p>{selectedLinkDescription}</p>
+                <div className-="modal-btn-container">
+                    {buttons.map((button) => (
+                        <button key={button.id} className="modal-btn" onClick={button.handler}>
+                            {button.name}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
