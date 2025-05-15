88a3ce80996750468e32f65250da43cd066a8b1e
TTahk <tomtahk27@gmail.com>
2025-05-15 17:59:39 -0600

    fleshing out skillicon implementation

--- frontend/src/components/SkillIcon.js
+++ frontend/src/components/SkillIcon.js
@@ -9,5 +9,12 @@ export default function makeSkillIcon(k, parent, posVec2, imageData, subtitle) {
         subtitle
     )
 
-    icon.use(k.area({shape: new.k}))
+    icon.use(k.area({shape: new k.Rect(k.vec2(0), icon.width + 50, icon.height + 65)}))
+    icon.use(k.body())
+    icon.onCollide("player", (player) => {
+        icon.applyImpulse(player.direction.scale(1000))
+    })
+    // TODO: opacity modifier for child game objects
+
+    return icon
 }
